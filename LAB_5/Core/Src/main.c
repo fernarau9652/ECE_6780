2835946e3f75561e184c547d5afecdd286b3379f
Fernando Araujo <fernarau9652@gmail.com>
2024-03-11 13:19:20 -0600

    initialized the I2C peripherals for TIMINGR and CR1

--- LAB_5/Core/Src/main.c
+++ LAB_5/Core/Src/main.c
@@ -21,6 +21,7 @@
 #include "stm32f072xb.h"
 
 void initLEDs(void);
+void initPeriph(void);
 
 
 void SystemClock_Config(void);
@@ -40,8 +41,13 @@ int main(void)
 	__HAL_RCC_GPIOB_CLK_ENABLE();
 	__HAL_RCC_I2C2_CLK_ENABLE();
 	
-	// initialize LEDs (used as a reference for functions)
+	// initialize GPIO modes and I2C peripherals
+	initPeriph();
+	
+	// initialize LEDs
 	initLEDs();
+	
+	
 
   /* USER CODE BEGIN SysInit */
 
@@ -68,8 +74,9 @@ int main(void)
   * @retval None
   */
 
+/* Initialize all configured peripherals */
 void initPeriph(void) {
-	/* Initialize all configured peripherals */
+	/* Setting the GPIO Modes */
 	// Set PB11 - AF1
 	GPIOB->MODER |= (GPIO_MODER_MODER11_1); // (Alternate function: 10)
 	//GPIOB->OTYPER |= (0x1 << GPIO_OTYPER_OT_11); // (Open-drain: 1)
@@ -91,12 +98,20 @@ void initPeriph(void) {
 	GPIOC->MODER |= (GPIO_MODER_MODER0_0); // (General purpose: 01)
 	GPIOC->OTYPER &= ~(GPIO_OTYPER_OT_0); // (Push-pull: 0)
 	HAL_GPIO_WritePin(GPIOC, GPIO_PIN_0, GPIO_PIN_SET); // set pin high
+
+	/* Initializing the I2C Peripheral */
+	// Set the parameters in the TIMINGR reg to use 100 kHz (section 26.7.5 Periph manual for mapping)
+	I2C2->TIMINGR |= (0x1  << I2C_TIMINGR_PRESC_Pos);  // PRESC = 1    [bits 28-31]
+	I2C2->TIMINGR |= (0x13 << I2C_TIMINGR_SCLL_Pos);   // SCLL = 0x13  [bits 0-7]
+	I2C2->TIMINGR |= (0x0F << I2C_TIMINGR_SCLH_Pos);   // SCHL = 0xF   [bits 8-15]
+	I2C2->TIMINGR |= (0x2  << I2C_TIMINGR_SDADEL_Pos); // SDADEL = 0x2 [bits 16-19]
+	I2C2->TIMINGR |= (0x4  << I2C_TIMINGR_SCLDEL_Pos); // SCLDEL = 0x4 [bits 20-23]
+	//I2C2->TIMINGR |= 0x10420F13; // Alternative approach
+	
+	I2C2->CR1 |= I2C_CR1_PE; // Peripheral enable
 }
 
-
-
-
-
+/* Initialize the LEDs*/
 void initLEDs(void) {	
 	/* Initialize all LEDs: RED (PC6), BLUE (PC7), ORANGE (PC8), GREEN (PC9)	*/ // /*
 	// (Reset state: 00)
@@ -106,7 +121,7 @@ void initLEDs(void) {
 	GPIOC->MODER |= (GPIO_MODER_MODER6_0 | GPIO_MODER_MODER7_0 | GPIO_MODER_MODER8_0 | GPIO_MODER_MODER9_0);
 	
 	// Configure Push/Pull Output type for PC6, PC7, PC8, and PC9	(00)
-	GPIOC->OTYPER &= ~(GPIO_OTYPER_OT_6 | GPIO_OTYPER_OT_7 | GPIO_MODER_MODER8_0 | GPIO_MODER_MODER9_0);
+	GPIOC->OTYPER &= ~(GPIO_OTYPER_OT_6 | GPIO_OTYPER_OT_7 | GPIO_OTYPER_OT_8 | GPIO_OTYPER_OT_9);
 	
 	// Configure low speed for PC6, PC7, PC8, and PC9	(00)
 	GPIOC->OSPEEDR &= ~(GPIO_OSPEEDR_OSPEEDR6_Msk | GPIO_OSPEEDR_OSPEEDR7_Msk | GPIO_OSPEEDR_OSPEEDR8_Msk | GPIO_OSPEEDR_OSPEEDR9_Msk);
